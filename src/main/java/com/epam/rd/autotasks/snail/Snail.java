61995d23cfee38153ad1967c09a52fc69203004d
Vitalii Kalinchyk <karlkory@gmail.com>
2022-07-07 20:38:00 +0300

    Snail

--- src/main/java/com/epam/rd/autotasks/snail/Snail.java
+++ src/main/java/com/epam/rd/autotasks/snail/Snail.java
@@ -1,12 +1,21 @@
 package com.epam.rd.autotasks.snail;
 
+import java.util.Scanner;
+
 public class Snail
 {
     public static void main(String[] args)
     {
-        //Write a program that reads a,b and h (line by lyne in this order) and prints
-        //the number of days for which the snail reach the top of the tree.
-        //a - feet that snail travels up each day, b - feet that slides down each night, h - height of the tree
-
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int h = input.nextInt();
+        if (a>=h) {
+            System.out.println(1);
+        } else if (a<=b){
+            System.out.println("Impossible");
+        } else {
+            System.out.println((h-a)/(a-b)+1);
+        }
     }
 }
